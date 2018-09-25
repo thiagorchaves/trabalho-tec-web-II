8c449ad77800c1c5fe9f6ee5c2f101ec9f12e552
thiagorchaves <thiago@qcx.com.br>
2018-09-25 16:33:41 -0300

    Add database_schema and change excluir.php

--- excluir.php
+++ excluir.php
@@ -1,7 +1,7 @@
 <?php
 $id = $_GET["id"];
 include 'conecta.php';
-$sql = "DELETE FROM projetos WHERE id='$id'";
+$sql = "DELETE FROM projects WHERE id='$id'";
 
 $resultado = mysqli_query($conexao,$sql) or die ("Nao foi possivel realizar a exclusao");
 echo "<h1>O projeto foi excluido com exito!</h1>";
