46e725b83e4a4403cbee2ae4f4382108e8a7d420
thiagorchaves <thiago@qcx.com.br>
2018-11-29 15:01:49 -0200

    Creating other table

--- banco/data_base_schema/database_schema.sql
+++ banco/data_base_schema/database_schema.sql
@@ -5,10 +5,6 @@ GRANT ALL PRIVILEGES ON * . * TO 'teste'@'%';
 CREATE TABLE projetos.project (
   id int(11) NOT NULL AUTO_INCREMENT,
   nome varchar(255) NOT NULL,
-  nome_projeto varchar(255) NOT NULL,
-  login varchar(255) NOT NULL,
-  password varchar (255) NOT NULL,  
-  email varchar (255) NOT NULL,
   description text NOT NULL,
   have_git tinyint (1) NOT NULL,
   online tinyint(1) NOT NULL,
@@ -21,3 +17,13 @@ CREATE TABLE projetos.project (
   createdate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (Id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+CREATE TABLE projetos.user (
+  user_id int(11) NOT NULL AUTO_INCREMENT,
+  nome varchar(255) NOT NULL,
+  login varchar(255) NOT NULL,
+  password varchar (255) NOT NULL,  
+  email varchar (255) NOT NULL,
+  createdate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (user_id)
+)  ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
